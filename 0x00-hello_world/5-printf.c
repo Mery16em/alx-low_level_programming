ea58ee31919f1a5f53f6d5c99406b62b10470fa8
Meryem <ml.meryem.16@gmail.com>
2024-06-22 10:43:55 -0700

    add comments

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
-/*
+
+/**
 * main - Entry point of the program
 *
 * This function prints a message with proper grammar and formatting.
 * It returns 0 upon successful execution.
 */
+
 int main(void)
 {
-/* Print the message with proper grammar and formatting */
+/* Print the message
+* with proper grammar 
+* and formatting 
+*/
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
