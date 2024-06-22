b960aa4e6f49d770cb72261bf12a8e4c2bbe92b6
Meryem <ml.meryem.16@gmail.com>
2024-06-22 10:58:18 -0700

    follow betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,13 +3,13 @@
 /**
  * main - Entry point of the program
  *
- * Description: Prints the message "Programming is like building a multilingual puzzle,"
+ * Description: Prints th a multilingual pu
  *              followed by a newline character.
  * Return: Always 0 (success)
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle,");
+	puts("Programming is like building a multilingual puzzle");
 	return (0);
 }
 
