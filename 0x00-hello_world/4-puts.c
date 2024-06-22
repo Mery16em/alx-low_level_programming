1a5066b63ceba3ac5114743c78d5d094e12dd1ac
Meryem <ml.meryem.16@gmail.com>
2024-06-22 10:55:18 -0700

    first try on puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints the message "Programming is like building a multilingual puzzle,"
+ *              followed by a newline character.
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle,");
+	return (0);
+}
+
