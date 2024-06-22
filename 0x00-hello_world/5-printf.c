df57e2e2f7691930e03d99cb686bc2a7b2b016ea
Meryem <ml.meryem.16@gmail.com>
2024-06-22 10:51:43 -0700

    betty again

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,17 +2,21 @@
 
 /**
 * main - Entry point of the program
-*
 * This function prints a message with proper grammar and formatting.
-* It returns 0 upon successful execution.
 */
 
 int main(void)
 {
-/* Print the message
-* with proper grammar 
-* and formatting 
+/*
+*  Print the message
+* with proper grammar
+* and formatting
 */
 printf("with proper grammar, but the outcome is a piece of art,\n");
+
+/*
+* Blbla
+* bla
+*/
 return (0);
 }
