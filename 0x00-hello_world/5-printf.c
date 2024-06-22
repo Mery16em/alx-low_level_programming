ab9cdbe0ed0dc193828abb65f7bfd4e346b8ac9f
Meryem <ml.meryem.16@gmail.com>
2024-06-22 08:57:38 -0700

    6th try on betty

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 
-/* Main function to print a message with proper grammar.
- * It returns 0 upon successful execution.
- */
 int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-
-    return 0;
+/* Print the message with proper grammar and formatting */
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
