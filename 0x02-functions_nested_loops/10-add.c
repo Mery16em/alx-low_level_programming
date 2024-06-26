5aadd8e1685c31080b744f872f7d94d39b808706
Meryem <ml.meryem.16@gmail.com>
2024-06-26 15:37:51 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int add(int a, int b);
+/*
+*  Declaration of the add function
+*/
+int main(void)
+{
+int result;
+
+result = add(5, 7);
+printf("Result: %d\n", result);
+
+return (0);
+}
