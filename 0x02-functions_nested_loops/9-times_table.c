18411a2a09e20f4ab1e7221214038c87c1e64022
Meryem <ml.meryem.16@gmail.com>
2024-06-26 15:25:32 -0700

    delete double

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,22 +8,6 @@ void times_table(void)
 {
 int i, j;
 
-for (i = 0; i <= 9; i++)
-{
-for (j = 0; j <= 9; j++)
-{
-printf("%d x %d = %d\n", i, j, i * j);
-}
-printf("\n")#include "main.h"
-#include <stdio.h>
-
-/**
- * times_table - Prints the 9 times table, starting from 0.
- */
-void times_table(void)
-{
-int i, j;
-
 /* Outer loop for rows */
 for (i = 0; i <= 9; i++)
 {
