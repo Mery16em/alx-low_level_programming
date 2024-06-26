72fd9d57895626bb7ab3c6717cde22c02eece9c1
Meryem <ml.meryem.16@gmail.com>
2024-06-26 15:03:13 -0700

    reduce num of character

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,11 +11,11 @@ int print_last_digit(int n)
 int last_digit;
 
 last_digit = n % 10;
-if (last_digit < 0) /* If the number is negative, convert last digit to positive */
+if (last_digit < 0) /* If its negative convert last digit to positive */
 last_digit = -last_digit;
 
-_putchar(last_digit + '0'); /* Print the last digit */
-    
-return last_digit;
+_putchar(last_digit + '0');/* Print the last digit */
+
+return (last_digit);
 }
 
