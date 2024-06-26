647e33c535eef8e02cd5cc0d4b7f8ef42c4cb491
Meryem <ml.meryem.16@gmail.com>
2024-06-26 15:22:04 -0700

    remove characters fro 80

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,7 +14,31 @@ for (j = 0; j <= 9; j++)
 {
 printf("%d x %d = %d\n", i, j, i * j);
 }
-printf("\n");
+printf("\n")#include "main.h"
+#include <stdio.h>
+
+/**
+ * times_table - Prints the 9 times table, starting from 0.
+ */
+void times_table(void)
+{
+int i, j;
+
+/* Outer loop for rows */
+for (i = 0; i <= 9; i++)
+{
+/* Inner loop for columns */
+for (j = 0; j <= 9; j++)
+{
+if (j == 0)
+printf("%2d", i * j); /* Print the fileading space */
+else
+printf(", %2d", i * j); /* Print comma and space */
+}
+printf("\n"); /* Move to the next line after each row is printed */
+}
+}
+;
 /**
 * Add a newline after each set of multiplications
 */
