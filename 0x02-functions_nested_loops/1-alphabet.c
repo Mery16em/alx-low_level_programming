4a866eaaa7a76050f5f6c556743e686c7ab06f1c
Meryem <ml.meryem.16@gmail.com>
2024-06-26 10:54:22 -0700

    remove space

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- * print_alphabet - Prints the alphabet in lowercase followed by a new line
- */
+* print_alphabet - Prints the alphabet in lowercase followed by a new line
+*/
 void print_alphabet(void)
 {
-    char letter = 'a';
+char letter = 'a';
 
-    while (letter <= 'z')
-    {
-        _putchar(letter);
-        letter++;
-    }
-    _putchar('\n');
+while (letter <= 'z')
+{
+_putchar(letter);
+letter++;
 }
+_putchar('\n');
+}
+i
