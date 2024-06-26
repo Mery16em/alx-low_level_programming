e14c0d346296c9b2bcec3f313ec27d77b3c80267
Meryem <ml.meryem.16@gmail.com>
2024-06-26 06:00:10 -0700

    0-putchart.c commit 7

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include "main.h"
-
-/* program to dispay putchar
-* */
+/**
+ * main - Entry point of the program
+ *
+ * This function prints "_putchar" followed by a newline and returns 0.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 printf("_putchar\n");
