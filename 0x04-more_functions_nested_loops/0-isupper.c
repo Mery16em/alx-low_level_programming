ba25fc75712b43b6e665d048badb4c68b80cc469
Meryem <ml.meryem.16@gmail.com>
2024-06-28 11:15:38 -0700

    add description betty style

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,24 @@
+/*
+ * Function: isupper
+ * ----------------------------
+ *   Checks if a character is an uppercase letter.
+ *
+ *   c: the character to be checked.
+ *
+ *   returns: 1 if the character is uppercase, 0 otherwise.
+ */
+
 int _isupper(int c)
+
+/*
+ * Function: isupper
+ * ----------------------------
+ *   Checks if a character is an uppercase letter.
+ *
+ *   c: the character to be checked.
+ *
+ *   returns: 1 if the character is uppercase, 0 otherwise.
+ */
 {
 if (c >= 'A' && c <= 'Z')
 {
