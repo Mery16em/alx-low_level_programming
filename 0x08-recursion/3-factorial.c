5f5fcf62875e671366232aebe37f42bd1966479e
Meryem <ml.meryem.16@gmail.com>
2024-07-10 13:38:29 -0700

    3 ad 4

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,14 +1,13 @@
-#include <stdio.h>
-
+/**
+ * factorial - returns the factorial of the number
+ * @n: the max factorial
+ * Return: factorial of number n
+ */
 int factorial(int n)
 {
-    if (n < 0)
-    {
-        return -1;
-    }
-    if (n == 0)
-    {
-        return 1;
-    }
-    return n * factorial(n - 
-
+	if (n < 0)
+		return (-1);
+	else if (n <= 1)
+		return (1);
+	return (n * factorial(n - 1));
+}
