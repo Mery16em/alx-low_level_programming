ee5a6bbb03246830e467ab478d2584bf908336bd
Meryem <ml.meryem.16@gmail.com>
2024-07-11 10:22:12 -0700

    prime

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int checker(int n, int base)
+{
+	if (base % n == 0 || base < 2)
+		return (0);
+	else if (n == base - 1)
+		return (1);
+	else if (base > n)
+		return (checker(n + 1, base));
+	return (1);
+}
+int is_prime_number(int n)
+{
+	return (checker(2, n));
+}
